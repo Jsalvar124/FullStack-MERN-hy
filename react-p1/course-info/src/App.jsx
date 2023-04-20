d72ae21d0aff34f787774dc5cdd5a1476d398fc4
Jsalvar124 <jsalvar124@gmail.com>
2023-04-20 10:53:55 -0500

    ex 1.5 Case

--- react-p1/course-info/src/App.jsx
+++ react-p1/course-info/src/App.jsx
@@ -85,25 +85,27 @@ export function Total (props) {
 
 //App Calling the created components with its props.
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
       <Total total={31}/>
     </div>
   )
